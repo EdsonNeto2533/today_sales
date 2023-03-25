bf81b4b0693648fe6e1598d28e952f3104fc6310
Edson <edsonneto2533@gmail.com>
2023-03-25 13:22:18 -0300

    feat: adjust imports and remove comments from code

--- core/lib/di/binds/core_binds.dart
+++ core/lib/di/binds/core_binds.dart
@@ -4,23 +4,6 @@ import 'package:core/sales_database.dart';
 import 'package:core/utils/constants/core_constants.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-// class CoreBinds {
-//   static final List<Bind> databaseBinds = [
-//     // Bind.lazySingleton(
-//     //   (i) async => await $FloorSalesDatabase
-//     //       .databaseBuilder(CoreConstants.databaseReference)
-//     //       .build(),
-//     // ),
-//     Bind.singleton<CollaboratorDAO>(
-//       (i) => i.get<SalesDatabase>().collaboratorDAO,
-//     ),
-//     Bind.singleton<SaleDAO>(
-//       (i) => i.get<SalesDatabase>().saleDAO,
-//     ),
-//     // AsyncBind<CollaboratorDAO>(((i) => teste.getDao()))
-//   ];
-// }
-
 class DatabaseBuilder {
   static CollaboratorDAO? collaboratorDAO;
   static SaleDAO? saleDAO;
--- dashboard/lib/di/viewmodel_binds.dart
+++ dashboard/lib/di/viewmodel_binds.dart
@@ -1,9 +1,3 @@
-import 'package:dashboard/dashboard_feature/domain/usecase/add_collaborator_use_case.dart';
-import 'package:dashboard/dashboard_feature/domain/usecase/get_collaborators_use_case.dart';
-import 'package:dashboard/dashboard_feature/domain/usecase/remove_collaborator_use_case.dart';
-import 'package:dashboard/dashboard_feature/presentation/usecase/iadd_collaborators_use_case.dart';
-import 'package:dashboard/dashboard_feature/presentation/usecase/iget_collaborators_use_case.dart';
-import 'package:dashboard/dashboard_feature/presentation/usecase/iremove_colaborator_use_case.dart';
 import 'package:dashboard/dashboard_feature/presentation/viewmodel/dashboard_collaborators_viewmodel.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
